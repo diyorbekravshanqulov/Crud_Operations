eef55d5934ed70e796eb6be9ea0cb13f424cf304
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-12 00:08:59 +0500

    complate

--- src/layouts/defaults.vue
+++ src/layouts/defaults.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Sidebar from '../components/Sidebar.vue'
+import Navbar from '../components/Navbar.vue'
 </script>
 
 <template>
   <div>
     <Sidebar />
+    <Navbar />
     <router-view />
   </div>
 </template>
