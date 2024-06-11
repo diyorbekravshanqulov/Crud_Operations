e500e5a946ae390ae2e86eb6882126a7b0ffc3b8
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-11 23:56:04 +0500

    complate

--- src/layouts/defaults.vue
+++ src/layouts/defaults.vue
@@ -1,8 +1,10 @@
 <script setup>
+import Sidebar from '../components/Sidebar.vue'
 </script>
 
 <template>
   <div>
+    <Sidebar />
     <router-view />
   </div>
 </template>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,10 +1,7 @@
 <script setup>
-// import Sidebar from "../components/Sidebar.js";
-import Sidebar from '../components/Sidebar.vue'
 </script>
 <template>
   <div>
-    <Sidebar />
-    <!-- <Sidebar /> -->
+    <router-view />
   </div>
 </template>
