b7b1b9f9a38dbb36d8072ae8e46fa3fcf260dd5f
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-12 09:47:45 +0500

    complate

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,23 @@
 <script setup>
+import { ref } from "vue";
+
+import { useRouter } from "vue-router";
+const user = ref(null);
+const router = useRouter();
+
+user.value = JSON.parse(localStorage.getItem("user"));
+
+console.log("user", user);
+
+if (!user) {
+  router.push("/login");
+}
 </script>
+
 <template>
   <div>
-    <router-view />
+    <div v-if="user">
+      <router-view />
+    </div>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,7 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from) => {
-  const isLoggedin = true;
+  const isLoggedin = JSON.parse(localStorage.getItem("user"));
 
   if (!isLoggedin && to.name != "login") {
     return { name: "login" };
