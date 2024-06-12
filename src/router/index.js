214e40775c6496c4c246498f6d2ce7484a61ba23
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-12 09:55:07 +0500

    complate

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(),
-  // redirect: '/login',
+  redirect: '/login',
   routes: [
     {
       path: "/",
